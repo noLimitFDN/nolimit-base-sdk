ae85538805096bbba029e9adedf011c1f41cb6da
noLimitFDN <faisalabri254@gmail.com>
2025-10-06 15:18:00 +0200

    types index

---
+++ src/types/index.ts
@@ -0,0 +1,35 @@
+/**
+ * Type exports for noLimit Base SDK
+ */
+
+export type {
+  NoLimitConfig,
+  Signer,
+  WalletClient,
+  TransactionRequest,
+  TransactionResponse,
+} from './config';
+
+export type {
+  ChatMessage,
+  ChatOptions,
+  ChatResponse,
+  StreamChunk,
+} from './chat';
+
+export type {
+  SwapParams,
+  SwapQuote,
+  SwapResult,
+  TokenInfo,
+} from './swap';
+
+export type {
+  MixParams,
+  MixResult,
+  MixStatus,
+  MixStatusType,
+} from './mixer';
+
+export type { SupportedToken } from './tokens';
+
